438eda185a7ab378de31ea6e87a0bd3559892a74
Ben Reinhart <benjreinhart@gmail.com>
2021-03-01 14:27:51 -0800

    Clean up fs

--- src/fs.ts
+++ src/fs.ts
@@ -2,36 +2,29 @@ import fs from "fs/promises";
 import fg from "fast-glob";
 import { constants } from "fs";
 
-export interface FS {
-  exists(path: string): Promise<boolean>;
-  read(path: string): Promise<string>;
-  glob(pattern: string): Promise<string[]>;
-  write(path: string, data: string): Promise<void>;
-  mkdirp(path: string): Promise<void>;
-}
-
 export default {
-  exists(path) {
-    return new Promise((resolve) => {
-      fs.access(path, constants.F_OK)
-        .then(() => resolve(true))
-        .catch(() => resolve(false));
-    });
+  async exists(path: string) {
+    try {
+      await fs.access(path, constants.F_OK);
+      return true;
+    } catch {
+      return false;
+    }
   },
 
-  read(path) {
+  read(path: string) {
     return fs.readFile(path, { encoding: "utf8" });
   },
 
-  glob(pattern) {
+  glob(pattern: string) {
     return fg(pattern);
   },
 
-  write(path, data) {
+  write(path: string, data: string) {
     return fs.writeFile(path, data, { encoding: "utf8" });
   },
 
-  mkdirp(path) {
+  mkdirp(path: string) {
     return fs.mkdir(path, { recursive: true });
   },
-} as FS;
+};
